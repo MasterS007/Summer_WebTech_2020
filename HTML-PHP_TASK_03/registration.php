fbc86a5da02b1a59fc9d698955bcd00c7be689a9
MasterS007 <munirasirajum13@gmail.com>
2020-07-23 02:28:33 +0600

    DOB Validated

--- HTML-PHP_TASK_03/registration.php
+++ HTML-PHP_TASK_03/registration.php
@@ -59,7 +59,7 @@
    else {
        echo "Invalid Email!";
    }
-   */
+   
 
    #Gender Validation
    
@@ -72,7 +72,25 @@
    else {
        echo "Successful Submit";
    }
+*/
 
+#DOB Validation
+$date = $_REQUEST['date'];
+$month = $_REQUEST['month'];
+$year = $_REQUEST['year'];
+
+if($date == NULL || $month==NULL || $year== NULL)
+{
+    echo "Invalid Date of Birth!";
+}
+
+elseif(($date>=1 && $date<=31) && ($month >=1 && $month<=12) && ($year >=1900 && $year <=2016))
+{
+    echo "Successful Submit!";
+}
+else {
+    echo "Invalid Date of Birth!";
+}
    
 ?>
    
