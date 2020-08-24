cf11efdb30a182832a9af75a14d9867a1df03f2f
MasterS007 <munirasirajum13@gmail.com>
2020-08-24 09:21:57 +0600

    php for email added

---
+++ FINAL_LAB_TASK_2/email/emailCheck.php
@@ -0,0 +1,23 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+        $email = $_REQUEST['email'];
+        $evalid = FALSE;
+        $pos =strpos($email, '@');
+        $pos1 = strpos($email, ".com");
+
+        if($email == NULL)
+        {
+            echo "Invalid Email!";
+        }
+
+        elseif ($pos!=False && $pos1!=False && $pos1 > $pos) {
+                
+                    echo "Submit Successful!";
+        }
+        else {
+            echo "Invalid Email!";
+        }
+    }
+?>
