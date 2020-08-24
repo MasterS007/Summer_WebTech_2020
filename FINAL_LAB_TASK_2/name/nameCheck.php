6919a2d85ff2003989043f764de9f5e0195c2391
MasterS007 <munirasirajum13@gmail.com>
2020-08-24 08:58:42 +0600

    js for name is added

--- FINAL_LAB_TASK_2/name/nameCheck.php
+++ FINAL_LAB_TASK_2/name/nameCheck.php
@@ -1,17 +1,45 @@
 <?php
+  
    
    if(isset($_POST['submit']))
-   {
+   {    
+    $name = $_REQUEST['name'];
+    $len = strlen($name);
+    
+    $nvalid = FALSE;
+    if($name == NULL || $len<2)
+    {
+     $nvalid = FALSE;
+    }
+ 
+    elseif($len >= 2 && $name[0]>='A' && $name[0]<='Z' || $name[0]>='a'&& $name[0]<='z')
+    {
+         for($i = 1; $i<$len; $i++)
+        {
+              if($name>='A'&& $name<='Z' || $name>='a'&& $name<='z' || $name=='.' || $name=='-' )
+              {
+                   $nvalid = TRUE;
+              }
+ 
+              else {
+                  $nvalid = FALSE;
+              }
+        }
+ 
+    }
+        
+    else {
+        $nvalid =FALSE;
+    }
+ 
     if($nvalid == TRUE)
     {
         echo "Submit Successful!";
     }
     else {
         echo "Invalid Name!";
-    }
+    } 
 
    }
 
-   
-  
    ?>
