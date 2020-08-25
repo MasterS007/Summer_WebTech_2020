b939f1abd6d1dd5ff920b7f454afe14a8e5884b6
MasterS007 <munirasirajum13@gmail.com>
2020-08-25 08:44:05 +0600

    email validated

--- FINAL_LABTASK_3/indexCheck.js
+++ FINAL_LABTASK_3/indexCheck.js
@@ -3,7 +3,7 @@
 window.nvalid=false;
 var i;
  
- function eMpty()
+ function neMpty()
  {
     var name = document.getElementById("name").value;
     var lent =name.length;
@@ -41,11 +41,40 @@ var i;
  }
 
  
-function Remover()
+function nRemover()
 {
     document.getElementById('nameMsg').innerHTML = "";
     
 }
+
+ function EeMpty()
+ {
+    var email = document.getElementById("email").value;
+    var pos =email.search("@");
+    var pos1 = email.search(".com");
+
+    if(email=="")
+    {
+       document.getElementById("emailMsg").innerHTML="field can't be empty";
+        window.evalid=false;
+    }
+    else if (pos==-1 || pos1==-1|| pos1 < pos) 
+    {
+        document.getElementById("emailMsg").innerHTML="invalid email must be like (sample@example.com)";
+        window.evalid=false;
+    }
+    else
+    {
+        window.evalid=true;
+    }
+ }
+
+ function eRemover()
+{
+    document.getElementById('emailMsg').innerHTML = "";
+    
+}
+
 
 function Validate()
 {
